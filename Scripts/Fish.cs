31e1ecc2e59d0ce2b8607d9ca2690b2f51f89ae5
Eren KALE <erenerenkale@gmail.com>
2023-05-29 19:32:19 +0300

    Updated Assets
    Updated Assets

--- Scripts/Fish.cs
+++ Scripts/Fish.cs
@@ -6,37 +6,37 @@ public class Fish : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float speed;
-    int angle, maxAngle = 20,minAngle=-60;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             _rb.velocity = new Vector2(_rb.velocity.x, speed);
         }
 
         if (_rb.velocity.y > 0)
         {
-            if (angle<maxAngle)
+            if (angle <= maxAngle)
             {
                 angle = angle + 4;
             }
         }
-        else if(_rb.velocity.y < -2.5f)
+        else if (_rb.velocity.y < -2.5f)
         {
             if (angle > minAngle)
             {
                 angle = angle - 2;
             }
         }
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-
-
+        transform.rotation = Quaternion.Euler(0, 0, angle); 
     }
 }
